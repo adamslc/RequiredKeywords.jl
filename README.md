709673029d4570660d75e87969845c41442a3cd5
Luke Adams <adamslc@whitman.edu>
2017-03-13 18:40:04 -0600

    Add project code

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # RequiredKeywords
 
+This package allows you to specify required keyword arguments.
+
+# Usage
+```julia
+julia> using RequiredKeywords
+
+julia> @required_keywords f(x; y::Int64) = x*y
+f (generic function with 1 method)
+
+julia> f(2,y=2)
+4
+
+julia> f(2)
+ERROR: Unassigned Keyword:  Required keyword y::Int64 not included.
+Stacktrace:
+ [1] f(::Int64) at ./REPL[5]:1
+```
+
 [![Build Status](https://travis-ci.org/adamslc/RequiredKeywords.jl.svg?branch=master)](https://travis-ci.org/adamslc/RequiredKeywords.jl)
 
 [![Coverage Status](https://coveralls.io/repos/adamslc/RequiredKeywords.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/adamslc/RequiredKeywords.jl?branch=master)
--- src/RequiredKeywords.jl
+++ src/RequiredKeywords.jl
@@ -1,5 +1,43 @@
 module RequiredKeywords
+    import Base: showerror
+    export UnassignedKeyword, @required_keywords, @showexp
 
-# package code goes here
+    struct UnassignedKeyword <:Exception
+        msg::String
+    end
+
+    Base.showerror(io::IO, e::UnassignedKeyword) = print(io, "Unassigned Keyword:  ", e.msg)
+
+    macro required_keywords(exp)
+        # Check if exp is a function definition
+        if exp.head == :function || (exp.head == :(=) && exp.args[1].head == :call)
+            # Find the list of parameters in function
+            paramListID = 0
+            for (i, arg) in enumerate(exp.args[1].args)
+                typeof(arg) == Symbol && continue
+                arg.head != :parameters && continue
+
+                paramListID = i
+            end
+
+            # Check each parameter, update if necessary
+            for (i, param) in enumerate(exp.args[1].args[paramListID].args)
+                if typeof(param) == Symbol || param.head == :(::)
+                    msg = "Required keyword $param not included."
+                    newParam = Expr(:kw, :($param), :( throw(UnassignedKeyword( $msg )) ))
+                    exp.args[1].args[paramListID].args[i] = newParam
+                end
+            end
+        else
+            throw("@required_keywords can only be applied to a function.")
+        end
+
+        # Return modified function definition
+        esc(:( $exp ))
+    end
+
+    macro showexp(exp)
+        show(exp)
+    end
 
 end # module
